df1bbdfca967f3f5b8099c9fa5b1c8004677e0a4
ErakabelIos <divjyotsingh@DJ.local>
2015-07-04 14:23:44 +0530

    Initial Commit

---
+++ SampleGitProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleGitProject
+//
+//  Created by Divjyot Singh on 04/07/15.
+//  Copyright (c) 2015 Divjyot Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleGitProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleGitProject
+//
+//  Created by Divjyot Singh on 04/07/15.
+//  Copyright (c) 2015 Divjyot Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
